8add8fb6233638d2569d39a91cc3801087094e1c
Yaoqi Dong <zczqydo@ucl.ac.uk>
2024-10-14 11:55:15 +0100

    do activity one

--- src/tutorialpkg/mypkg1/mymodule1.py
+++ src/tutorialpkg/mypkg1/mymodule1.py
@@ -2,6 +2,7 @@
 
 from tutorialpkg.mypkg2.mymodule2_1 import calculate_area_of_circle
 from tutorialpkg.mypkg2.mymodule2_2 import fetch_user_data
+from pathlib import Path
 
 mock_database = {
     1: {'name': 'Alice', 'email': 'alice@example.com', 'age': 30},
@@ -15,7 +16,15 @@ if __name__ == '__main__':
     area = calculate_area_of_circle(10)
     print(f"The area is {area}.")
 
-    # Use the fetch_user_data(user_id, database) function to print the data for the user with ID 42 that is in `mock_database` variable.
+    # Use the fetch_user_data(user_id, database) function to print the data
+    # for the user with ID 42 that is in `mock_database` variable.
     print(fetch_user_data(42, mock_database))
 
-    # Locate the data file `paralmpics_raw.csv` relative to this file using pathlib.Path. Prove it exists.
+    # Locate the data file `paralmpics_raw.csv` relative to this file using
+    # pathlib.Path. Prove it exists.
+
+csv_file = Path(__file__).parent.parent.joinpath('data', 'paralympics_events_raw.csv')
+if csv_file.exists():
+    print(f"CSV file found: {csv_file}")
+else:
+    print("CSV file not found.")
